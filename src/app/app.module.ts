472c2c1dda816c9e1b107e8d81d85707ce4b819c
Nantenaina Rafidy~ <nantyrafidy@gmail.com>
2023-01-14 00:23:58 +0300

    Home page client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ClientHomeComponent } from './client-home/client-home.component';
+import { ServicesCarouselComponent } from './services-carousel/services-carousel.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroBannerComponent,
+    ClientHomeComponent,
+    ServicesCarouselComponent,
+    TestimonialsComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { faPhoneVolume, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  faPhoneVolume = faPhoneVolume;
+  faLocationDot = faLocationDot;
+}
