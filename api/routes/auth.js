4a5814a130094c26ee305357613f109af4de9624
Steve <ramiandrisoasteve@gmail.com>
2023-01-16 22:19:25 +0300

    envois email avec token - validation email

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const dbo = require("../db/connect");
+var nodemailer = require('nodemailer');
+var nodeoutlook = require('nodejs-nodemailer-outlook');
 
 router.get("/test-db", async function (req, res) {
   const db = dbo.getDb();
@@ -33,18 +35,88 @@ router.post("/test-db/create", function (req, res) {
 
 router.post("/inscription", function (req, res)  {
   const dbConnect = dbo.getDb();
+  
+  // console.log(fullUrl);
   let user = {
       firstname : req.body.firstname,
       lastname : req.body.lastname,
       email : req.body.email,
       password : req.body.password,
-      type: 'client'
+      type: 'client',
+      validationEmail : '0'
   }
-  dbConnect.collection("User").insertOne(user, function (err, result) {
+
+  dbConnect.collection("User")                                                      //check doublon d'emails
+    .find({email: req.body.email})
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(500).json("Error fetching records!");
+      } else {
+        if(result.length>0){
+          res.status(500).json({'message' : "cette email est deja relié a un compte"});
+        }
+      }
+    });
+
+    
+
+  dbConnect.collection("User").insertOne(user, function (err, result) {                   //create user
       if (err) {
         res.status(500).json("Error inserting matches!");
       } else {
-        res.status(200).json(result);
+
+        const date = new Date();
+        date.setDate(date.getDate() + 1);
+        // console.log("here");
+        console.log(result['insertedId'].toString());
+        let token = {
+            userId : result['insertedId'].toString(),
+            datePeremption : date
+        }
+
+        dbConnect.collection("token").insertOne(token, function (err, result1) {    //generer token pour inscription
+
+          if (err) {
+              res.status(500).json("Error inserting matches!");
+          } else {
+            console.log('----------------'+result1["insertedId"]);
+              let tok = {
+                  _id : result1["insertedId"]
+              }
+              let tokenFinal = dbConnect.collection("token")
+              .find(tok)
+              .toArray(function (err, result) {
+                if (err) {
+                  res.status(500).json("Error fetching records!");
+                } else {
+                  var fullUrl = req.protocol + '://' + req.get('host')+'/api/user/validerEmail/'+result1["insertedId"].toString();
+                  console.log('here');
+                  nodeoutlook.sendEmail({                                                           //envois email
+                    tls: {
+                      rejectUnauthorized: false
+                    },
+                    auth: {
+                      user: "m1p10mean-nantenaina-steve@outlook.com",
+                      pass: "projetITU2023",
+                      
+                    },
+                      from: "m1p10mean-nantenaina-steve@outlook.com",
+                      to: req.body.email,
+                      subject: 'validation d\'email',
+                      html: 'Veuillez valider votre email en cliquant <a href= \"'+fullUrl+'\">ici</a>',
+                      text: 'This is text version!',
+                      replyTo: "",
+                      onError: (e) => console.log(e),
+                      onSuccess: (i) => res.status(200).json("Email envoyé")
+                    });
+                  res.status(200).json(result);
+                }
+              });
+          }
+          });
+
+        
+        // res.status(200).json(result);
       }
     });
 });
@@ -62,16 +134,25 @@ router.post("/login", function (req, res)  {
     if (err) {
       res.status(500).json("Error fetching records!");
     } else {
+
+      if(result[0]['validationEmail'] == '0'){
+        res.status(400).json({'message': 'email non valider'})
+      }
+
       const date = new Date();
-      console.log(date);
       date.setDate(date.getDate() + 5);
-      console.log(date.getDate());
-      
+
       let token = {
           user : result[0],
           datePeremption : date
       }
+      // var resu = generateToken(token,dbConnect,res);
+      // // console.log( generateToken(token,dbConnect,res));
+      // console.log(resu);
+      // // res.status(resu['status']).json(resu['value'])
+      // res.status(200)
       dbConnect.collection("token").insertOne(token, function (err, result1) {
+
       if (err) {
           res.status(500).json("Error inserting matches!");
       } else {
@@ -93,5 +174,35 @@ router.post("/login", function (req, res)  {
     }
   });
 });
+
+function generateToken(token,dbConnect,res) {
+
+  return dbConnect.collection("token").insertOne(token, function (err, result1) {
+    
+    if (err) {
+        res.status(500).json("Error inserting matches!");
+    } else {
+      // console.log(result1["insertedId"]);
+        let tok = {
+            _id : result1["insertedId"]
+        }
+        return dbConnect.collection("token")
+        .find(tok)
+        .toArray(function (err, result) {
+          if (err) {
+            // res.status(500).json("Error fetching records!");
+            return {status: 500,
+            value: "Error fetching records!"}
+          } else {
+            // res.status(200).json(result);
+            let retour = {status: 200,
+                        value: result}
+                        console.log(retour);
+            return retour;
+          }
+        });
+    }
+    });
+}
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -38,6 +38,60 @@ router.get("/me", async function (req, res) {
     
   });
 
+  router.get("/validerEmail/:id", async function (req, res) {
+    const db = dbo.getDb();
+    const id = req.params.id;
+    let token = {
+      _id : mongoose.Types.ObjectId(id)
+    }
+
+    db.collection("token")
+    .find(token)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(500).json("Error fetching records!");
+      } else {
+        
+        if(result.length > 0) {
+            var idu = result[0]['userId'];
+            let user = {
+              _id : mongoose.Types.ObjectId(idu)
+            }
+      
+          db.collection("User")
+          .find(user)
+          .toArray(function (err, result) {
+            if (err) {
+              res.status(500).json("Error fetching records!");
+            } else {
+              
+              if(result.length > 0) {
+                  // var user = result[0];
+                  // user['validationEmail'] = '1';
+                  console.log(result[0]);
+                  let myquery = result[0];
+                  let newvalues = {$set: {validationEmail : '1'}};
+                  db.collection("User").updateOne(myquery, newvalues, function(err, res) {
+                    if (err) throw err;
+                    console.log("email user valider");
+                    // db.close();
+                  });
+                  res.status(200);
+              }else{
+                  res.status(500).json({'message':'user introuvable'})
+              }
+      
+            }
+          });
+            res.status(200).json(user);
+        }else{
+            res.status(500).json({'message':'user introuvable'})
+        }
+
+      }
+    });
+  })
+
   router.get("/:id", async function (req, res) {
     
     const db = dbo.getDb();
@@ -47,7 +101,7 @@ router.get("/me", async function (req, res) {
     let user = {
         _id : mongoose.Types.ObjectId(id)
     }
-    
+
     db.collection("User")
     .find(user)
     .toArray(function (err, result) {
@@ -57,7 +111,7 @@ router.get("/me", async function (req, res) {
         
         if(result.length > 0) {
             var user = result[0];
-            res.status(200).json(user);
+            res.status(200).json('email user valider');
         }else{
             res.status(500).json({'message':'user introuvable'})
         }
@@ -66,5 +120,7 @@ router.get("/me", async function (req, res) {
     });
     
   });
+
+  
 
 module.exports = router;
