c9535d3607249804e581344f3637c5d608bfa91b
Nantenaina Rafidy~ <nantyrafidy@gmail.com>
2023-01-18 08:54:18 +0300

    appconfig

--- src/app/appconfig/appconfig.service.ts
+++ src/app/appconfig/appconfig.service.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from "@angular/core";
-import { environment } from "../env/env.dev";
+import { environment } from "../../environments/environment.development";
 import { Appconfig } from "../interfaces/appconfig.interface";
 
 export const APP_SERVICE_CONFIG = new InjectionToken<Appconfig>('app.config');
