d6716016dde74873df90af5c5d2ef74526c2b0dc
Steve <ramiandrisoasteve@gmail.com>
2023-01-30 09:47:45 +0300

    statistique api ca par mois
    /api/statistique/camois/:mois/:annee

--- api/routes/statistique.js
+++ api/routes/statistique.js
@@ -36,6 +36,41 @@ router.get("/cajour/:date", async function (req, res) {
 });
 
 
+router.get("/camois/:mois/:annee", async function (req, res) {
+    try {
+        const db = await dbo.getDb();
+        var mois = req.params.mois;
+        var annee = req.params.annee;
+
+        var start = new Date(annee + "-" + mois + "-01T00:00:00.000+00:00");
+        var end = new Date(annee + "-" + mois + "-31T23:59:59.999+00:00");
+
+        db.collection("paiement").aggregate([
+            {
+                $match: {
+                    "date": {
+                        $gte: start,
+                        $lt: end
+                    }
+                }
+            },
+        ]).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            var total = 0.0;
+            for(let i = 0; i < result.length; i++){
+                console.log(i);
+                total+= parseFloat(result[i].montant);
+            }
+            return res.status(200).json({'total':total});
+        });
+    }catch (err) { 
+        console.log(err);
+        return res.status(500).json({ message: "error while doing a paiement" });
+    }
+    return res.status(200);
+});
+
 
 
 module.exports = router;
